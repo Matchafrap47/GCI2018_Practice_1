6aa11e70a735dd938eebc281ee689394972b460b
Matchafrap47 <44703500+Matchafrap47@users.noreply.github.com>
2018-11-02 21:53:58 +0800

    Beginner: Learn a bit of python by doing a few simple exercises - CCExtractor Development

---
+++ GCI2018_Practice_1.py
@@ -0,0 +1,9 @@
+count = 0
+
+while (count < 10 ):
+    print ('GCI is great')
+    count = count + 1
+
+name = input ('Could you please tell me your name ? ')
+print('Hello',name,',please to meet you')
+print('Hello',name,', please to meet you! Did you know that your name backwards is',name,'?')
