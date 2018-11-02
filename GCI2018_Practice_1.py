12cbdfca6a6eef1a8982a0d3c6574f0aaa327f24
Matchafrap47 <matchafrap47@outlook.com>
2018-11-02 22:42:26 +0800

    Add files via upload

--- GCI2018_Practice_1.py
+++ GCI2018_Practice_1.py
@@ -5,5 +5,6 @@ while (count < 10 ):
     count = count + 1
 
 name = input ('Could you please tell me your name ? ')
+name2 = name[::-1]
 print('Hello',name,',please to meet you')
-print('Hello',name,', please to meet you! Did you know that your name backwards is',name,'?')
+print('Hello',name,', please to meet you! Did you know that your name backwards is',name2,'?')
